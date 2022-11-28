7384a7f794e2fbef156c5768b2f7a2d00a71967a
Angel <angelSistemas21@gmail.com>
2022-11-27 21:53:38 -0600

    feat: Added HTML elements with createElement, innerHTML and appendChild to display tasks dynamically

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const btn = document.querySelector("[data-form-btn]");
 // console.log(btn)
 
 const createTask = (evento) => {
-    // El formulario por defecto tiene el funcionamiento de refrescar la página o trata de enviar los datos a alguna parte
     evento.preventDefault();
-    const input = document.querySelector("[data-form-input]"); 
-    console.log(input.value);   
+    const input = document.querySelector('[data-form-input]'); 
+    const list = document.querySelector('[data-list]');
+    const value = input.value;
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+    list.appendChild(task);
 }
 
 btn.addEventListener("click", createTask );
